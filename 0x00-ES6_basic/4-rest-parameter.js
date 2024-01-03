118a597828bbcb6d849d291a4c02b173459b96a8
mblevu <brianislevu@gmail.com>
2024-01-04 01:09:59 +0300

    completing es6 basics

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-export default function returnHowManyArguments() {
-    return arguments.length;
+export default function returnHowManyArguments(...args) {
+    return args.length;
 }
