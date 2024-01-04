888c8111b7d16f9f50bc34f1936196f7ad9b5197
mblevu <brianislevu@gmail.com>
2024-01-05 01:24:48 +0300

    fixing unkept promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve();
-      } else {
-        reject(new Error('Error fetching data from API'));
-      }
-    }, 2000);
+    resolve(true);
+    reject(new Error('Error fetching API'));
   });
 }
