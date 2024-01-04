6278c1e9e19b08780b6b47d9bfc68b0debddcb62
mblevu <brianislevu@gmail.com>
2024-01-05 01:48:08 +0300

    handling promises

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  }).catch(() => {
+    console.log('Got a response from the API');
+    return new Error();
+  }).finally(() => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
+}
