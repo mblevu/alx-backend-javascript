36bbbcd4059bb0e9e3fe0b2c3bdc27ef2c9f36dd
mblevu <brianislevu@gmail.com>
2024-01-05 11:32:24 +0300

    handling division by 0

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export default function divideFunction(numerator, denominator) {
+  return new Promise((resolve, reject) => {
+    if (denominator === 0) {
+      reject(new Error('cannot divide by 0'));
+    } else {
+      resolve(numerator / denominator);
+    }
+  });
+}
