9b533d1cc49cfdedfa026d5378f39a901c0762cc
mblevu <brianislevu@gmail.com>
2024-01-10 01:16:53 +0300

    first push

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+export default function getListStudents() {
+  return [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
+}
