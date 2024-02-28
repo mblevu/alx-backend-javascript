a7ae72d43f763640cdd5ac07d6a3f093077cbc8a
mblevu <brianislevu@gmail.com>
2024-02-28 12:30:17 +0300

    pass lint and function logs right message

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,9 +1,9 @@
 const fs = require('fs').promises;
 
-function countStudents (path) {
+function countStudents(path) {
   return fs.readFile(path, 'utf8')
     .then((data) => {
-      const lines = data.split('\n');
+      const lines = data.split('\n').slice(1);
       const students = lines.filter((line) => line).map((line) => line.split(','));
 
       console.log(`Number of students: ${students.length}`);
@@ -18,7 +18,9 @@ function countStudents (path) {
       }
 
       for (const field in fields) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
       }
     })
     .catch(() => {
