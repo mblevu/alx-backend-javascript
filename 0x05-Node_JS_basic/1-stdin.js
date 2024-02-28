1732a953ab753333b36e26f24c8c25af10f45377
mblevu <brianislevu@gmail.com>
2024-02-28 12:06:13 +0300

    fix eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.once('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing');
-    process.exit(0);
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit(0);
 });
 
 process.on('exit', () => {
-    process.stdout.write('\n');
+  process.stdout.write('This important software is now closing\n');
 });
